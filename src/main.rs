80f13f1d914f5cc3e1f161c42009262a4bd9afaf
valadaa48 <valadaa48@gmx.com>
2020-06-11 23:35:29 -0400

    release

--- src/main.rs
+++ src/main.rs
@@ -2,120 +2,112 @@ extern crate evdev_rs as evdev;
 
 use evdev::*;
 use evdev::enums::*;
-use std::io;
 use std::fs::File;
 use std::path::Path;
 use std::process::Command;
+use std::{thread, time};
 
-//fn print_abs_bits(dev: &Device, axis: &EV_ABS) {
-//
-//    let code = EventCode::EV_ABS(axis.clone());
-//
-//	if !dev.has(&code) { return; }
-//
-//	let abs = dev.abs_info(&code).unwrap();
-//
-//	println!("	Value	{}", abs.value);
-//	println!("	Min	{}", abs.minimum);
-//	println!("	Max	{}", abs.maximum);
-//	if abs.fuzz != 0 {
-//		println!("	Fuzz	{}", abs.fuzz);
-//    }
-//	if abs.flat != 0 {
-//		println!("	Flat	{}", abs.flat);
-//    }
-//	if abs.resolution != 0 {
-//		println!("	Resolution	{}", abs.resolution);
-//    }
-//}
+static HOTKEY:      EventCode = EventCode::EV_KEY(EV_KEY::BTN_TRIGGER_HAPPY3);
+static BRIGHT_UP:   EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_UP);
+static BRIGHT_DOWN: EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_DOWN);
+static VOL_UP:      EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_RIGHT);
+static VOL_DOWN:    EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_LEFT);
+static PERF_MAX:    EventCode = EventCode::EV_KEY(EV_KEY::BTN_TR);
+static PERF_NORM:   EventCode = EventCode::EV_KEY(EV_KEY::BTN_TL);
 
-//fn print_code_bits(dev: &Device, ev_code: &EventCode, max: &EventCode) {
-//    for code in ev_code.iter() {
-//        if code == *max {
-//            break;
-//        }
-//        if !dev.has(&code) {
-//            continue;
-//        }
-//
-//		println!("    Event code: {}", code);
-//        match code {
-//            EventCode::EV_ABS(k) => print_abs_bits(dev, &k),
-//            _ => ()
-//        }
-//    }
-//}
+fn handle_js(dev: &Device, ev: &InputEvent) {
+//    println!("Event: time {}.{}, type {} , code {} , value {}",
+//             ev.time.tv_sec,
+//             ev.time.tv_usec,
+//             ev.event_type,
+//             ev.event_code,
+//             ev.value);
 
-//fn print_bits(dev: &Device) {
-//    println!("Supported events:");
-//
-//    for ev_type in  EventType::EV_SYN.iter() {
-//		if dev.has(&ev_type) {
-//			println!("  Event type: {} ", ev_type);
-//        }
-//
-//        match ev_type {
-//            EventType::EV_KEY => print_code_bits(dev, &EventCode::EV_KEY(EV_KEY::KEY_RESERVED),
-//                                                 &EventCode::EV_KEY(EV_KEY::KEY_MAX)),
-//            EventType::EV_REL => print_code_bits(dev, &EventCode::EV_REL(EV_REL::REL_X),
-//                                                 &EventCode::EV_REL(EV_REL::REL_MAX)),
-//            EventType::EV_ABS => print_code_bits(dev, &EventCode::EV_ABS(EV_ABS::ABS_X),
-//                                                 &EventCode::EV_ABS(EV_ABS::ABS_MAX)),
-//            EventType::EV_LED => print_code_bits(dev, &EventCode::EV_LED(EV_LED::LED_NUML),
-//                                                 &EventCode::EV_LED(EV_LED::LED_MAX)),
-//            _ => (),
-//		}
-//	}
-//}
+    if ev.event_type != EventType::EV_KEY || ev.value != 1 {
+        return;
+    }
 
-//fn print_props(dev: &Device) {
-//	println!("Properties:");
-//
-//	for input_prop in InputProp::INPUT_PROP_POINTER.iter() {
-//		if dev.has(&input_prop) {
-//			println!("  Property type: {}", input_prop);
-//        }
-//    }
-//}
+    if dev.event_value(&HOTKEY) != Some(1) { return; }
 
-static HOTKEY: EventCode = EventCode::EV_KEY(EV_KEY::BTN_TRIGGER_HAPPY3);
-
-fn handle_event(dev: &Device, ev: &InputEvent) {
-//    if ev.event_type != EventType::EV_KEY {
-//        return;
-//    }
-
-    let f3 = dev.event_value(&HOTKEY);
-
-    println!("Event: time {}.{}, type {} , code {} , value {} f3 {}",
-             ev.time.tv_sec,
-             ev.time.tv_usec,
-             ev.event_type,
-             ev.event_code,
-             ev.value,
-             f3.unwrap());
-
-    if ev.event_code == EventCode::EV_KEY(EV_KEY::BTN_TRIGGER_HAPPY5) {
-        println!("F5 was pressed");
+    if ev.event_code == BRIGHT_UP {
+        Command::new("light").args(&["-T","1.1"]).output().expect("Failed to execute light");
+        Command::new("light").arg("-O").output().expect("Failed to execute light");
+    }
+    else if ev.event_code == BRIGHT_DOWN {
+        Command::new("light").args(&["-T","0.9"]).output().expect("Failed to execute light");
+        Command::new("light").arg("-O").output().expect("Failed to execute light");
+    }
+    else if ev.event_code == VOL_UP {
+        Command::new("amixer").args(&["-q", "sset", "Playback", "1%+"]).output().expect("Failed to execute amixer");
+    }
+    else if ev.event_code == VOL_DOWN {
+        Command::new("amixer").args(&["-q", "sset", "Playback", "1%-"]).output().expect("Failed to execute amixer");
+    }
+    else if ev.event_code == PERF_MAX {
+        Command::new("performance").arg("on").output().expect("Failed to execute performance");
+    }
+    else if ev.event_code == PERF_NORM {
+        Command::new("performance").arg("off").output().expect("Failed to execute performance");
     }
 }
 
 fn main() {
-    let f = File::open(Path::new("/dev/input/event2")).unwrap();
+    Command::new("light").arg("-I").output().expect("Failed to execute light");
+    let mut dev_pwr = Device::new().unwrap();
+    dev_pwr.set_fd(File::open(Path::new("/dev/input/event0")).unwrap()).unwrap();
+
+    let mut dev_hp = Device::new().unwrap();
+    dev_hp.set_fd(File::open(Path::new("/dev/input/event1")).unwrap()).unwrap();
 
     let mut dev_js = Device::new().unwrap();
-    dev_js.set_fd(f).unwrap();
+    dev_js.set_fd(File::open(Path::new("/dev/input/event2")).unwrap()).unwrap();
 
-    let mut a: io::Result<(ReadStatus, InputEvent)>;
+    let repeat_rate = time::Duration::from_millis(100);
+
     loop {
-        a = dev_js.next_event(evdev::ReadFlag::NORMAL | evdev::ReadFlag::BLOCKING);
-        if a.is_ok() {
-            let result = a.ok().unwrap();
-            if result.0 == ReadStatus::Success {
-                handle_event(&dev, &result.1);
+        while dev_js.has_event_pending() {
+            let a = dev_js.next_event(evdev_rs::ReadFlag::NORMAL);
+            match a {
+                Ok(k) => {
+                    handle_js(&dev_js, &k.1);
+                },
+                _ => ()
             }
-        } else {
-            println!("Error");
         }
+
+        if dev_hp.has_event_pending() {
+            let a = dev_hp.next_event(evdev_rs::ReadFlag::NORMAL);
+            match a {
+                Ok(k) => {
+                    let ev = &k.1;
+                    if ev.event_code == EventCode::EV_SW(EV_SW::SW_HEADPHONE_INSERT) {
+                        let mut dest = "HP";
+                        if ev.value == 1 { dest = "SPK" }
+                        Command::new("amixer").args(&["-q", "sset", "'Playback Path'", dest]).output().expect("Failed to execute amixer");
+                    }
+                },
+                _ => ()
+            }
+        }
+
+        if dev_pwr.has_event_pending() {
+            let a = dev_pwr.next_event(evdev_rs::ReadFlag::NORMAL);
+            match a {
+                Ok(k) => {
+                    let ev = &k.1;
+                    if ev.event_code == EventCode::EV_KEY(EV_KEY::KEY_POWER) && ev.value == 1 {
+                        if dev_js.event_value(&HOTKEY) == Some(1) {
+                            Command::new("sudo").args(&["systemctl", "poweroff"]).output().expect("Failed to execute systemctl");
+                        }
+                        else {
+                            Command::new("sudo").args(&["systemctl", "suspend"]).output().expect("Failed to execute systemctl");
+                        }
+                    }
+                },
+                _ => ()
+            }
+        }
+
+        thread::sleep(repeat_rate);
     }
 }
