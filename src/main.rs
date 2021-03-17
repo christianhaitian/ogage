f4726cd364f25c5f60fcec725a935a3867e55067
christianhaitian <mckman@comcast.net>
2021-03-16 23:58:07 -0400

    Updated for ArkOS RG351P/M Edition

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,15 @@ use std::os::unix::io::AsRawFd;
 use mio::{Poll,Events,Token,Interest};
 use mio::unix::SourceFd;
 
-static HOTKEY:      EventCode = EventCode::EV_KEY(EV_KEY::BTN_TRIGGER_HAPPY5);
-static BRIGHT_UP:   EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_UP);
-static BRIGHT_DOWN: EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_DOWN);
-static VOL_UP:      EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_RIGHT);
-static VOL_DOWN:    EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_LEFT);
-static PERF_MAX:    EventCode = EventCode::EV_KEY(EV_KEY::BTN_TR);
-static PERF_NORM:   EventCode = EventCode::EV_KEY(EV_KEY::BTN_TL);
+static HOTKEY:      EventCode = EventCode::EV_KEY(EV_KEY::BTN_TR2);
+static BRIGHT:   EventCode = EventCode::EV_ABS(EV_ABS::ABS_HAT0Y);
+//static VOL:      EventCode = EventCode::EV_ABS(EV_ABS::ABS_HAT0X);
+static PERF_MAX:    EventCode = EventCode::EV_KEY(EV_KEY::BTN_Z);
+static PERF_NORM:   EventCode = EventCode::EV_KEY(EV_KEY::BTN_WEST);
 //static DARK_ON:     EventCode = EventCode::EV_KEY(EV_KEY::BTN_TR2);
 //static DARK_OFF:    EventCode = EventCode::EV_KEY(EV_KEY::BTN_TL2);
-static VOLUME_UP:   EventCode = EventCode::EV_KEY(EV_KEY::KEY_VOLUMEUP);
-static VOLUME_DOWN: EventCode = EventCode::EV_KEY(EV_KEY::KEY_VOLUMEDOWN);
 
-fn blink1() {
+/*fn blink1() {
     Command::new("brightnessctl").arg("-O").output().expect("Failed to execute brightnessctl");
 
     Command::new("brightnessctl").args(&["-T","1.5"]).output().expect("Failed to execute brightnessctl");
@@ -45,32 +41,32 @@ fn blink2() {
     Command::new("sleep").arg("0.1").output().expect("Failed to execute brightnessctl");
 
     Command::new("brightnessctl").arg("-I").output().expect("Failed to execute brightnessctl");
-}
+}*/
 
 fn process_event(_dev: &Device, ev: &InputEvent, hotkey: bool) {
-//    println!("Event: time {}.{} type {} code {} value {} hotkey {}",
-//             ev.time.tv_sec,
-//             ev.time.tv_usec,
-//             ev.event_type,
-//             ev.event_code,
-//             ev.value,
-//             hotkey);
+    /*println!("Event: time {}.{} type {} code {} value {} hotkey {}",
+             ev.time.tv_sec,
+             ev.time.tv_usec,
+             ev.event_type,
+             ev.event_code,
+             ev.value,
+             hotkey);*/
 
-    if hotkey && ev.value == 1 {
-        if ev.event_code == BRIGHT_UP {
-            Command::new("brightnessctl").args(&["s","+5%"]).output().expect("Failed to execute brightnessctl");
+    if hotkey{
+        if ev.event_code == BRIGHT && ev.value == -1 {
+            Command::new("brightnessctl").args(&["s","+2%"]).output().expect("Failed to execute brightnessctl");
             //Command::new("brightnessctl").arg("-O").output().expect("Failed to execute brightnessctl");
         }
-        else if ev.event_code == BRIGHT_DOWN {
-            Command::new("brightnessctl").args(&["s","5%-"]).output().expect("Failed to execute brightnessctl");
+        else if ev.event_code == BRIGHT && ev.value == 1 {
+            Command::new("brightnessctl").args(&["-n15","s","2%-"]).output().expect("Failed to execute brightnessctl");
             //Command::new("brightnessctl").arg("-O").output().expect("Failed to execute brightnessctl");
         }
-        else if ev.event_code == VOL_UP {
+        /*else if ev.event_code == VOL && ev.value == 1 {
             Command::new("amixer").args(&["-q", "sset", "Playback", "1%+"]).output().expect("Failed to execute amixer");
         }
-        else if ev.event_code == VOL_DOWN {
+        else if ev.event_code == VOL && ev.value == -1 {
             Command::new("amixer").args(&["-q", "sset", "Playback", "1%-"]).output().expect("Failed to execute amixer");
-        }
+        }*/
         else if ev.event_code == PERF_MAX {
             Command::new("sudo").arg("perfmax").output().expect("Failed to execute performance");
             //blink1();
@@ -83,29 +79,23 @@ fn process_event(_dev: &Device, ev: &InputEvent, hotkey: bool) {
             //blink2();
             Command::new("sudo").args(&["systemctl", "poweroff"]).output().expect("Failed to execute power off");
         }
-        //else if ev.event_code == DARK_ON {
-            //Command::new("sudo").args(&["rfkill", "block", "all"]).output().expect("Failed to execute rfkill");
-            //blink1();
-        //}
-        //else if ev.event_code == DARK_OFF {
-            //Command::new("sudo").args(&["rfkill", "unblock", "all"]).output().expect("Failed to execute rfkill");
-            //blink1();
-        //}
+        /*else if ev.event_code == DARK_ON {
+            Command::new("sudo").args(&["rfkill", "block", "all"]).output().expect("Failed to execute rfkill");
+            blink1();
+        }
+        else if ev.event_code == DARK_OFF {
+            Command::new("sudo").args(&["rfkill", "unblock", "all"]).output().expect("Failed to execute rfkill");
+            blink1();
+        }*/
     }
     else if ev.event_code == EventCode::EV_SW(EV_SW::SW_HEADPHONE_INSERT) {
         let dest = match ev.value { 1 => "SPK", _ => "HP" };
         Command::new("amixer").args(&["-q", "sset", "'Playback Path'", dest]).output().expect("Failed to execute amixer");
-        blink1();
+        //blink1();
     }
     else if ev.event_code == EventCode::EV_KEY(EV_KEY::KEY_POWER) && ev.value == 1 {
-        blink2();
+        //blink2();
         Command::new("sudo").args(&["systemctl", "suspend"]).output().expect("Failed to execute suspend");
-    }
-    else if ev.event_code == VOLUME_UP {
-        Command::new("amixer").args(&["-q", "sset", "Playback", "1%+"]).output().expect("Failed to execute amixer");
-    }
-    else if ev.event_code == VOLUME_DOWN {
-        Command::new("amixer").args(&["-q", "sset", "Playback", "1%-"]).output().expect("Failed to execute amixer");
     }
 }
 
@@ -116,7 +106,7 @@ fn main() -> io::Result<()> {
     let mut hotkey = false;
 
     let mut i = 0;
-    for s in ["/dev/input/event3", "/dev/input/event2", "/dev/input/event0", "/dev/input/event1"].iter() {
+    for s in ["/dev/input/event2", "/dev/input/event0", "/dev/input/event1"].iter() {
         if !Path::new(s).exists() {
             println!("Path {} doesn't exist", s);
             continue;
@@ -143,7 +133,7 @@ fn main() -> io::Result<()> {
                     Ok(k) => {
                         let ev = &k.1;
                         if ev.event_code == HOTKEY {
-                            hotkey = ev.value == 1;
+                            hotkey = ev.value == 1 || ev.value == 2;
                             //let grab = if hotkey { GrabMode::Grab } else { GrabMode::Ungrab };
                             //dev.grab(grab)?;
                         }
