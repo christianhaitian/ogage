8cc1cd4d561491943c396be89aa214003b413177
Christian_Haitian <mckman@comcast.net>
2022-06-23 23:04:54 +0000

    Update volume controls, add bluetooth on/off to Fn key, minor cleanup

--- src/main.rs
+++ src/main.rs
@@ -14,36 +14,11 @@ use mio::unix::SourceFd;
 static HOTKEY:      EventCode = EventCode::EV_KEY(EV_KEY::BTN_THUMBR);
 static BRIGHT_UP:   EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_UP);
 static BRIGHT_DOWN: EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_DOWN);
+static QVOL_UP:     EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_RIGHT);
+static QVOL_DN:     EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_LEFT);
 static VOL_UP:      EventCode = EventCode::EV_KEY(EV_KEY::KEY_VOLUMEUP);
 static VOL_DN:      EventCode = EventCode::EV_KEY(EV_KEY::KEY_VOLUMEDOWN);
-//static PERF_MAX:    EventCode = EventCode::EV_KEY(EV_KEY::BTN_Z);
-//static PERF_NORM:   EventCode = EventCode::EV_KEY(EV_KEY::BTN_WEST);
-//static DARK_ON:     EventCode = EventCode::EV_KEY(EV_KEY::BTN_TR2);
-//static WIFI_T:    EventCode = EventCode::EV_KEY(EV_KEY::BTN_TL2);
-
-/*fn blink1() {
-    Command::new("brightnessctl").arg("-O").output().expect("Failed to execute brightnessctl");
-
-    Command::new("brightnessctl").args(&["-T","1.5"]).output().expect("Failed to execute brightnessctl");
-    Command::new("sleep").arg("0.1").output().expect("Failed to execute brightnessctl");
-
-    Command::new("brightnessctl").arg("-I").output().expect("Failed to execute brightnessctl");
-}
-
-fn blink2() {
-    Command::new("brightnessctl").arg("-O").output().expect("Failed to execute brightnessctl");
-
-    Command::new("brightnessctl").args(&["-T","1.5"]).output().expect("Failed to execute brightnessctl");
-    Command::new("sleep").arg("0.1").output().expect("Failed to execute brightnessctl");
-
-    Command::new("brightnessctl").arg("-I").output().expect("Failed to execute brightnessctl");
-    Command::new("sleep").arg("0.1").output().expect("Failed to execute brightnessctl");
-
-    Command::new("brightnessctl").args(&["-T","1.5"]).output().expect("Failed to execute brightnessctl");
-    Command::new("sleep").arg("0.1").output().expect("Failed to execute brightnessctl");
-
-    Command::new("brightnessctl").arg("-I").output().expect("Failed to execute brightnessctl");
-}*/
+static BT_TRG:      EventCode = EventCode::EV_KEY(EV_KEY::BTN_MODE);
 
 fn process_event(_dev: &Device, ev: &InputEvent, hotkey: bool) {
     /*println!("Event: time {}.{} type {} code {} value {} hotkey {}",
@@ -71,47 +46,32 @@ fn process_event(_dev: &Device, ev: &InputEvent, hotkey: bool) {
             Command::new("brightnessctl").args(&["-n2","s","1%-"]).output().expect("Failed to execute brightnessctl");
             //Command::new("brightnessctl").arg("-O").output().expect("Failed to execute brightnessctl");
         }
-        /*else if ev.event_code == VOL && ev.value == 1 {
-            Command::new("amixer").args(&["-q", "sset", "Playback", "1%+"]).output().expect("Failed to execute amixer");
+        else if ev.event_code == QVOL_UP && ev.value > 0 {
+            Command::new("amixer").args(&["-q", "sset", "Master", "1%+"]).output().expect("Failed to execute amixer");
         }
-        else if ev.event_code == VOL && ev.value == -1 {
-            Command::new("amixer").args(&["-q", "sset", "Playback", "1%-"]).output().expect("Failed to execute amixer");
-        }*/
-        /*else if ev.event_code == PERF_MAX {
-            Command::new("sudo").args(&["perfmax", "On"]).output().expect("Failed to execute performance");
-            //blink1();
+        else if ev.event_code == QVOL_DN && ev.value > 0 {
+            Command::new("amixer").args(&["-q", "sset", "Master", "1%-"]).output().expect("Failed to execute amixer");
         }
-        else if ev.event_code == PERF_NORM {
-            Command::new("sudo").arg("perfnorm").output().expect("Failed to execute performance");
-            //blink1();
-        }*/
         else if ev.event_code == EventCode::EV_KEY(EV_KEY::KEY_POWER) {
             //blink2();
             Command::new("sudo").args(&["systemctl", "poweroff"]).output().expect("Failed to execute power off");
         }
-        /*else if ev.event_code == DARK_ON {
-            Command::new("sudo").args(&["rfkill", "block", "all"]).output().expect("Failed to execute rfkill");
-            blink1();
-        }
-        else if ev.event_code == WIFI_T && ev.value == 1 {
-            Command::new("sudo").arg("wifitoggle.sh").output().expect("Failed to execute wifitoggle.sh");
-            //blink1();
-        }*/
     }
     else if ev.event_code == EventCode::EV_SW(EV_SW::SW_HEADPHONE_INSERT) {
         let dest = match ev.value { 1 => "SPK", _ => "HP" };
         Command::new("amixer").args(&["-q", "sset", "'Playback Path'", dest]).output().expect("Failed to execute amixer");
-        //blink1();
     }
     else if ev.event_code == EventCode::EV_KEY(EV_KEY::KEY_POWER) && ev.value == 1 {
-        //blink2();
         Command::new("sudo").args(&["systemctl", "suspend"]).output().expect("Failed to execute suspend");
     }
     else if ev.event_code == VOL_UP && ev.value > 0 {
-         Command::new("amixer").args(&["-q", "sset", "Master", "1%+"]).output().expect("Failed to execute amixer");
+         Command::new("amixer").args(&["-q", "sset", "Master", "1+"]).output().expect("Failed to execute amixer");
     }
     else if ev.event_code == VOL_DN && ev.value > 0 {
-         Command::new("amixer").args(&["-q", "sset", "Master", "1%-"]).output().expect("Failed to execute amixer");
+         Command::new("amixer").args(&["-q", "sset", "Master", "1-"]).output().expect("Failed to execute amixer");
+    }
+    else if ev.event_code == BT_TRG && ev.value > 0 {
+         Command::new("sudo").arg("bttoggle.sh").output().expect("Failed to execute bttoggle.sh");
     }
 }
 
@@ -122,7 +82,7 @@ fn main() -> io::Result<()> {
     let mut hotkey = false;
 
     let mut i = 0;
-    for s in ["/dev/input/event3", "/dev/input/event2", "/dev/input/event0", "/dev/input/event1"].iter() {
+    for s in ["/dev/input/event10", "/dev/input/event9", "/dev/input/event8", "/dev/input/event7", "/dev/input/event6", "/dev/input/event5", "/dev/input/event4", "/dev/input/event3", "/dev/input/event2", "/dev/input/event1", "/dev/input/event0"].iter() {
         if !Path::new(s).exists() {
             println!("Path {} doesn't exist", s);
             continue;
@@ -136,8 +96,6 @@ fn main() -> io::Result<()> {
         i += 1;
     }
 
-    //Command::new("brightnessctl").arg("-I").output().expect("Failed to execute brightnessctl");
-
     loop {
         poll.poll(&mut events, None)?;
 
@@ -150,8 +108,6 @@ fn main() -> io::Result<()> {
                         let ev = &k.1;
                         if ev.event_code == HOTKEY {
                             hotkey = ev.value == 1 || ev.value == 2;
-                            //let grab = if hotkey { GrabMode::Grab } else { GrabMode::Ungrab };
-                            //dev.grab(grab)?;
                         }
                         process_event(&dev, &ev, hotkey)
                     },
