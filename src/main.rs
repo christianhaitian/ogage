c90e785845730f37a9e6ce7a10822c9c2db23cbb
christianhaitian <mckman@comcast.net>
2023-07-22 21:10:56 -0400

    Update main.rs to change hotkey to R3

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::os::unix::io::AsRawFd;
 use mio::{Poll,Events,Token,Interest};
 use mio::unix::SourceFd;
 
-static HOTKEY:      EventCode = EventCode::EV_KEY(EV_KEY::BTN_MODE);
+static HOTKEY:      EventCode = EventCode::EV_KEY(EV_KEY::BTN_THUMBR);
 static BRIGHT_UP:   EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_UP);
 static BRIGHT_DOWN: EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_DOWN);
 static QVOL_UP:     EventCode = EventCode::EV_KEY(EV_KEY::BTN_DPAD_RIGHT);
